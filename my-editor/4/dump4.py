849d7e2ac6921fcf764f0ccbec6b39f7ec9816d3
Nobody Fear <nobodyf34r@gmail.com>
2023-09-10 22:02:24 +0800

    bug fix

--- my-editor/4/dump4.py
+++ my-editor/4/dump4.py
@@ -719,28 +719,3 @@ def main(file, edit):
             if original_equipment_amount - len(equipmentlist) > 0:
                 f.seek(103587+63*j)
                 f.write(b"\x00"*63*(original_equipment_amount - len(equipmentlist)))
-
-
-def edit(position, location, money, namelist, gatcharemaining, gatchamax, characterlist, yokailist, itemlist, specialsoullist, yokaisoullist, equipmentlist):
-    namelist = {'nate': 'Nate', 'katie': 'Katie', 'summer': 'Summer', 'cole': 'Cole', 'bruno': 'Bruno', 'jack': 'Jack'} #fix names
-    
-    for i in range(len(yokailist)):
-        yokailist = edit_yokai(yokailist, i, "shogunyan", "") #get id from data.py if it isn't working
-
-    for i in range(6): #TODO make sure there are always 6
-        characterlist = edit_yokai(characterlist, i) #max stats
-
-
-    print(", ".join([characters[i["id"]]for i in characterlist]))
-    print(", ".join([characters[i["id"]]for i in yokailist]))
-    print(", ".join([items[i["item"]]for i in itemlist]))
-    print(", ".join([equipments[i["equipment"]]for i in equipmentlist]))
-    print(locations[location])
-
-    return position, location, money, namelist, gatcharemaining, gatchamax, characterlist, yokailist, itemlist, specialsoullist, yokaisoullist, equipmentlist
-
-
-infile = "/Users/emilia/Documents/dev/ykw/ykw-editors/my-editor/4/0/USERDATA00/data copy.bin"
-# infile = "/Volumes/UNTITLED/switch/Checkpoint/saves/0x010086C00AF7C000 0x010086C00AF7C000/0 copy/USERDATA00/data.bin"
-
-main(infile, edit)
