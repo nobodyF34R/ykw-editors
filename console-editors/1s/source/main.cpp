aa3ed05d6fd26d757c34edc3a19a39d1bbcb0747
Nobody Fear <nobodyf34r@gmail.com>
2023-10-20 16:52:50 +0800

    all variables

--- console-editors/1s/source/main.cpp
+++ console-editors/1s/source/main.cpp
@@ -107,10 +107,6 @@ int main(int argc, char** argv) {
                     if (file == NULL) {
                         std::cout << "Failed to open file " << filePath << "." << std::endl;
                     } else {
-                        // fseek(file, 0, SEEK_END);
-                        // long fileSize = ftell(file);
-                        // fseek(file, 0, SEEK_SET);
-
                         char buffer[256];
                         size_t bytesRead;
 
@@ -134,22 +130,22 @@ int main(int argc, char** argv) {
 
                         //EDIT HERE
                         
-                        uint32_t* x = reinterpret_cast<uint32_t*>(& decryptedData[20]);
-                        uint32_t* y = reinterpret_cast<uint32_t*>(& decryptedData[24]);
-                        uint32_t* z = reinterpret_cast<uint32_t*>(& decryptedData[28]);
+                        uint32_t* x = (uint32_t*)(&decryptedData[20]);
+                        uint32_t* y = (uint32_t*)(& decryptedData[24]);
+                        uint32_t* z = (uint32_t*)(& decryptedData[28]);
 
-                        uint64_t* location = reinterpret_cast<uint64_t*>(& decryptedData[112]);
+                        uint64_t* location = (uint64_t*)(& decryptedData[112]);
 
-                        uint16_t* time = reinterpret_cast<uint16_t*>(& decryptedData[1752]);
-                        uint8_t* sun = reinterpret_cast<uint8_t*>(& decryptedData[1754]);
+                        uint16_t* time = (uint16_t*)(& decryptedData[1752]);
+                        uint8_t* sun = (uint8_t*)(& decryptedData[1754]);
 
-                        uint32_t* money = reinterpret_cast<uint32_t*>(& decryptedData[37620]); 
+                        uint32_t* money = (uint32_t*)(& decryptedData[37620]); 
 
 
                         int16_t offset;
 
 
-                        std::vector<std::map<std::string, int>> yokailist;
+                        std::vector<Yokai> yokailist;
                         offset = 7696;
                         for (int i = 0; i < 240; i++) {
 
@@ -157,56 +153,23 @@ int main(int argc, char** argv) {
                                 break;
                             }
 
-                            std::map<std::string, int> dictionary;
-                            dictionary["num1"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset]);
-                            dictionary["num2"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset+2]);
-                            dictionary["yokai"] = *reinterpret_cast<uint32_t*>(&decryptedData[offset+4]);
-                            //nickname TODO
-                            dictionary["attack"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+78]);
-                            dictionary["technique"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+82]);
-                            dictionary["soultimate"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+86]);
-                            dictionary["xp"] = *reinterpret_cast<uint32_t*>(&decryptedData[offset+88]);
-                            dictionary["hp"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset+92]);
-                            dictionary["soul"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset+94]);
-                            dictionary["tiv_hp"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+96]);
-                            dictionary["tiv_str"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+97]);
-                            dictionary["tiv_spr"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+98]);
-                            dictionary["tiv_def"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+99]);
-                            dictionary["tiv_spd"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+100]);
-                            //half bytes TODO
-                            dictionary["ev_hp"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+106]);
-                            dictionary["ev_str"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+107]);
-                            dictionary["ev_spr"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+108]);
-                            dictionary["ev_def"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+109]);
-                            dictionary["ev_spd"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+110]);
-                            dictionary["level"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+116]);
-                            dictionary["attitude"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+117]);
-
-                            yokailist.push_back(dictionary);
+                            yokailist.push_back(Yokai(decryptedData, offset));
                             offset += 124;
                         }
-                        const int8_t original_yokai_amount = yokailist.size();
 
-                        std::vector<std::map<std::string, int>> itemlist;
+                        std::vector<Item> itemlist;
                         offset = 1784;
                         for (int i = 0; i < 256; i++) {
 
                             if (decryptedData[offset+2] == 0) {
                                 break;
                             }
-
-                            std::map<std::string, int> dictionary;
-                            dictionary["num1"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset]);
-                            dictionary["num2"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset+2]);
-                            dictionary["item"] = *reinterpret_cast<uint32_t*>(&decryptedData[offset+4]);
-                            dictionary["amount"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+8]);
                             
-                            itemlist.push_back(dictionary);
+                            itemlist.push_back(Item(decryptedData, offset));
                             offset += 12;
                         }
-                        const int8_t original_item_amount = itemlist.size();
 
-                        std::vector<std::map<std::string, int>> equipmentlist;
+                        std::vector<Equipment> equipmentlist;
                         offset = 4868;
                         for (int i = 0; i < 100; i++) {
 
@@ -214,18 +177,11 @@ int main(int argc, char** argv) {
                                 break;
                             }
 
-                            std::map<std::string, int> dictionary;
-                            dictionary["num1"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset]);
-                            dictionary["num2"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset+2]);
-                            dictionary["equipment"] = *reinterpret_cast<uint32_t*>(&decryptedData[offset+4]);
-                            dictionary["amount"] = *reinterpret_cast<uint8_t*>(&decryptedData[offset+8]);
-
-                            equipmentlist.push_back(dictionary);
+                            equipmentlist.push_back(Equipment(decryptedData, offset));
                             offset += 12;
                         }
-                        const int8_t original_equipment_amount = equipmentlist.size();
 
-                        std::vector<std::map<std::string, int>> importantlist;
+                        std::vector<Important> importantlist;
                         offset = 6480;
                         for (int i = 0; i < 150; i++) {
 
@@ -233,47 +189,41 @@ int main(int argc, char** argv) {
                                 break;
                             }
 
-                            std::map<std::string, int> dictionary;
-                            dictionary["num1"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset]);
-                            dictionary["num2"] = *reinterpret_cast<uint16_t*>(&decryptedData[offset+2]);
-                            dictionary["important"] = *reinterpret_cast<uint32_t*>(&decryptedData[offset+4]);
-
-                            importantlist.push_back(dictionary);
+                            importantlist.push_back(Important(decryptedData, offset));
                             offset += 8;
                         }
-                        const int8_t original_important_amount = importantlist.size();
 
                         // std::vector<int> medalliumlist; //TODO
                         // offset = 1476;
                         // importantlist.push_back();
 
-                        // printf("\nYokai:");
+                        printf("\nYokai:");
 
-                        // for (int i = 0; i < yokailist.size(); i++) {
-                        //     printf("\n%d: %u", i, yokailist[i]["yokai"]);
-                        // }
+                        for (int i = 0; i < yokailist.size(); i++) {
+                            printf("\n%d: %u", i, *yokailist[i].yokai);
+                        }
 
-                        // printf("\n\nItems:");
+                        printf("\n\nItems:");
 
-                        // for (int i = 0; i < itemlist.size(); i++) {
-                        //     printf("\n%d: %u", i, itemlist[i]["item"]);
-                        // }
+                        for (int i = 0; i < itemlist.size(); i++) {
+                            printf("\n%d: %u", i, *itemlist[i].item);
+                        }
 
-                        // printf("\n\nEquipment:");
+                        printf("\n\nEquipment:");
 
-                        // for (int i = 0; i < equipmentlist.size(); i++) {
-                        //     printf("\n%d: %u", i, equipmentlist[i]["equipment"]);
-                        // }
+                        for (int i = 0; i < equipmentlist.size(); i++) {
+                            printf("\n%d: %u", i, *equipmentlist[i].equipment);
+                        }
 
-                        // printf("\n\nImportant:");
+                        printf("\n\nImportant:");
 
-                        // for (int i = 0; i < importantlist.size(); i++) {
-                        //     printf("\n%d: %u", i, importantlist[i]["important"]);
-                        // }
+                        for (int i = 0; i < importantlist.size(); i++) {
+                            printf("\n%d: %u", i, *importantlist[i].important);
+                        }
                         
                         bool save = true;
 
-                        printf("\n+ to save and exit, - to exit without saving, A to increment money.");
+                        printf("\n+ to save and exit, - to exit without saving \nA to increment money, B to set all yokai to togenyan");
                         printf("\nmoney: %u", *money);
                         while (appletMainLoop()) {
                             padUpdate(&pad);
@@ -290,6 +240,12 @@ int main(int argc, char** argv) {
                                 (*money)++;
                                 printf("\nmoney: %u", *money);
                             }
+                            if (kDown & HidNpadButton_B){
+                                for (int i = 0; i < yokailist.size(); i++) {
+                                    *yokailist[i].yokai = 72463062;
+                                }
+                                printf("\nall yokai set to togenyan");
+                            }
 
                             consoleUpdate(NULL);
                         }
@@ -297,13 +253,13 @@ int main(int argc, char** argv) {
                         
                         if (save) {
                             fseek(file, 0, SEEK_SET);
-                            fwrite(yw_proc(decryptedData, true).data(), 1, decryptedData.size(), file); //could hardcode 47564 or use fileSize
+                            fwrite(yw_proc(decryptedData, true).data(), 1, 47564, file);
                             fclose(file);
 
                             std::string bakfilePath = filePath;
                             bakfilePath.replace(bakfilePath.rfind(".yw"), 3, ".bak");
                             FILE* bakfile = fopen(bakfilePath.c_str(), "wb");
-                            fwrite(encryptedData.data(), 1, encryptedData.size(), bakfile);
+                            fwrite(encryptedData.data(), 1, 47564, bakfile);
                             fclose(bakfile);
                             fsdevCommitDevice("save");
                         }
--- console-editors/1s/source/structs.h
+++ console-editors/1s/source/structs.h
@@ -3,38 +3,109 @@
 
 #include <iostream>
 
-// Define the Yokai struct to store the extracted data
 struct Yokai {
-    uint16_t num1;
-    uint16_t num2;
-    uint32_t yokai;
-    // std::string nickname[68];
-    uint8_t attack;
-    uint8_t technique;
-    uint8_t soultimate;
-    uint32_t xp;
-    uint16_t hp;
-    uint8_t soul;
+    uint16_t* num1;
+    uint16_t* num2;
+    uint32_t* yokai;
+    // char* nickname; //TODO
+    uint8_t* attack;
+    uint8_t* technique;
+    uint8_t* soultimate;
+    uint32_t* xp;
+    uint16_t* hp;
+    uint8_t* soul;
     struct {
-        uint8_t tiv_hp;
-        uint8_t tiv_str;
-        uint8_t tiv_spr;
-        uint8_t tiv_def;
-        uint8_t tiv_spd;
-        uint8_t iv21_hp;
-        uint8_t iv21_str;
-        uint8_t iv21_spr;
-        uint8_t iv21_def;
-        uint8_t iv21_spd;
-        uint8_t ev_hp;
-        uint8_t ev_str;
-        uint8_t ev_spr;
-        uint8_t ev_def;
-        uint8_t ev_spd;
+        uint8_t* tiv_hp;
+        uint8_t* tiv_str;
+        uint8_t* tiv_spr;
+        uint8_t* tiv_def;
+        uint8_t* tiv_spd;
+        uint8_t* iv21_hp; // TODO nibbles
+        uint8_t* iv21_str;
+        uint8_t* iv21_spr;
+        uint8_t* iv21_def;
+        uint8_t* iv21_spd;
+        uint8_t* ev_hp;
+        uint8_t* ev_str;
+        uint8_t* ev_spr;
+        uint8_t* ev_def;
+        uint8_t* ev_spd;
     } stats;
-    uint8_t level;
-    uint8_t attitude;
+    uint8_t* level;
+    uint8_t* attitude;
     // TODO: Add fields for "loaf level," "held item," and "affection"
+
+    // Construct given decryptedData and offset
+    Yokai(std::vector<uint8_t>& decryptedData, int16_t offset) {
+        num1 = (uint16_t*)(&decryptedData[offset]);
+        num2 = (uint16_t*)(&decryptedData[offset+2]);
+        yokai = (uint32_t*)(&decryptedData[offset+4]);
+        // ignore nickname for now
+        attack = &decryptedData[offset+78];
+        technique = &decryptedData[offset+82];
+        soultimate = &decryptedData[offset+86];
+        xp = (uint32_t*)(&decryptedData[offset+88]);
+        hp = (uint16_t*)(&decryptedData[offset+92]);
+        soul = &decryptedData[offset+94];
+        stats.tiv_hp = &decryptedData[offset+96];
+        stats.tiv_str = &decryptedData[offset+97];
+        stats.tiv_spr = &decryptedData[offset+98];
+        stats.tiv_def = &decryptedData[offset+99];
+        stats.tiv_spd = &decryptedData[offset+100];
+        stats.iv21_hp = &decryptedData[offset+101];
+        stats.iv21_str = &decryptedData[offset+102];
+        stats.iv21_spr = &decryptedData[offset+103];
+        stats.iv21_def = &decryptedData[offset+104];
+        stats.iv21_spd = &decryptedData[offset+105];
+        stats.ev_hp = &decryptedData[offset+106];
+        stats.ev_str = &decryptedData[offset+107];
+        stats.ev_spr = &decryptedData[offset+108];
+        stats.ev_def = &decryptedData[offset+109];
+        stats.ev_spd = &decryptedData[offset+110];
+        level = &decryptedData[offset+116];
+        attitude = &decryptedData[offset+117];
+    }
+};
+
+struct Item {
+    uint16_t* num1;
+    uint16_t* num2;
+    uint32_t* item;
+    uint8_t* amount;
+
+    Item(std::vector<uint8_t>& decryptedData, int16_t offset) {
+        num1 = (uint16_t*)(&decryptedData[offset]);
+        num2 = (uint16_t*)(&decryptedData[offset+2]);
+        item = (uint32_t*)(&decryptedData[offset+4]);
+        amount = &decryptedData[offset+8];
+    }
+};
+
+struct Equipment {
+    uint16_t* num1;
+    uint16_t* num2;
+    uint32_t* equipment;
+    uint32_t* amount;
+
+    Equipment(std::vector<uint8_t>& decryptedData, int16_t offset) {
+        num1 = (uint16_t*)(&decryptedData[offset]);
+        num2 = (uint16_t*)(&decryptedData[offset+2]);
+        equipment = (uint32_t*)(&decryptedData[offset+4]);
+        amount = (uint32_t*)(&decryptedData[offset+8]);
+    }
+};
+
+
+struct Important{
+    uint16_t* num1;
+    uint16_t* num2;
+    uint32_t* important;
+
+    Important(std::vector<uint8_t>& decryptedData, int16_t offset) {
+        num1 = (uint16_t*)(&decryptedData[offset]);
+        num2 = (uint16_t*)(&decryptedData[offset+2]);
+        important = (uint32_t*)(&decryptedData[offset+4]);
+    }
 };
 
 #endif // STRUCTS_H
